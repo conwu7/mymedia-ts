0c49cba7115d882bf2e8fb12508a1ef2cabfcb80
Clinton Onwu <clintononwu@yahoo.com>
2021-11-06 11:33:57 -0500

    revert UniversalModal to modal from bottom

--- src/components/utils/universalModal/UniversalModal.tsx
+++ src/components/utils/universalModal/UniversalModal.tsx
@@ -1,21 +1,47 @@
-import { IconButton, SwipeableDrawer } from '@mui/material';
+import { Backdrop, Fade, IconButton, Modal } from '@mui/material';
 import Box from '@mui/material/Box';
 import { MdOutlineClose } from 'react-icons/all';
 import style from './style.module.scss';
 import { UniversalModalProps } from './types';
 
+// export default function UniversalModal({ isOpen, onClose, children, title }: UniversalModalProps): JSX.Element {
+//   return (
+//     <SwipeableDrawer open={isOpen} onClose={onClose} onOpen={() => undefined} anchor="bottom" className={style.drawer}>
+//       <Box className={style.modalContainer}>
+//         <Box className={style.modalHeader}>
+//           <header className={style.modalTitle}>{title}</header>
+//           <IconButton className={style.closeModalButtonWrapper} onClick={onClose}>
+//             <MdOutlineClose />
+//           </IconButton>
+//         </Box>
+//         <Box className={style.modalContentContainer}>{children}</Box>
+//       </Box>
+//     </SwipeableDrawer>
+//   );
+// }
+
 export default function UniversalModal({ isOpen, onClose, children, title }: UniversalModalProps): JSX.Element {
   return (
-    <SwipeableDrawer open={isOpen} onClose={onClose} onOpen={() => undefined} anchor="bottom" className={style.drawer}>
-      <Box className={style.modalContainer}>
-        <Box className={style.modalHeader}>
-          <header className={style.modalTitle}>{title}</header>
-          <IconButton className={style.closeModalButtonWrapper} onClick={onClose}>
-            <MdOutlineClose />
-          </IconButton>
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={isOpen}>
+        <Box className={style.modalContainer}>
+          <Box className={style.modalHeader}>
+            <header className={style.modalTitle}>{title}</header>
+            <IconButton className={style.closeModalButtonWrapper} onClick={onClose}>
+              <MdOutlineClose />
+            </IconButton>
+          </Box>
+          <Box className={style.modalContentContainer}>{children}</Box>
         </Box>
-        <Box className={style.modalContentContainer}>{children}</Box>
-      </Box>
-    </SwipeableDrawer>
+      </Fade>
+    </Modal>
   );
 }
