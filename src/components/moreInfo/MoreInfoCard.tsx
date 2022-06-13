fbbb9ddcb277ea8298a392de0f94910c28be5016
Clinton Onwu <clintononwu@yahoo.com>
2022-06-13 10:26:25 -0500

    Merge more info and review card

--- src/components/moreInfo/MoreInfoCard.tsx
+++ src/components/moreInfo/MoreInfoCard.tsx
@@ -1,113 +1,58 @@
 import { Link } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import { useState } from 'react';
-import defaultPoster from '../../images/default-poster.png';
 import style from './style.module.scss';
-import {
-  HandleTabChange,
-  MediaReviewCardProps,
-  MoreInfoCardProps,
-  MoreInfoMedia,
-  MoreInfoTabs,
-  MoreInfoTabsDisplay,
-} from './types';
+import { MediaReview, MoreInfoCardProps, MoreInfoMedia } from './types';
 
 export default function MoreInfoCard({ hideReviewTab, media, userMedia }: MoreInfoCardProps): JSX.Element {
-  const [currentTab, setCurrentTab] = useState<MoreInfoTabs>('info');
-  const handleTabChange: HandleTabChange = (event, newTab) => setCurrentTab(newTab);
   return (
     <div className={style.mediaInfoCard}>
-      <AppBar className={style.tabSwitcherContainer}>
-        <Tabs value={currentTab} onChange={handleTabChange} centered>
-          <Tab label={MoreInfoTabsDisplay.info} value="info" className={style.tab} />
-          {!hideReviewTab && <Tab label={MoreInfoTabsDisplay.review} value="review" className={style.tab} />}
-        </Tabs>
-      </AppBar>
       <Box>
-        {currentTab === 'info' && <MediaInfoCard media={media} />}
-        {currentTab === 'review' && <MediaReviewCard userMedia={userMedia} />}
+        <MediaInfoCard media={media} userMedia={userMedia} />
       </Box>
     </div>
   );
 }
 
-function MediaInfoCard({ media }: { media: MoreInfoMedia }): JSX.Element {
-  const {
-    posterUrl,
-    language,
-    plot,
-    title,
-    runtime,
-    runYears,
-    imdbID,
-    totalSeasons,
-    genre,
-    imdbRating,
-    releaseDate,
-    actors,
-  } = media;
+function MediaInfoCard({ media, userMedia }: { media: MoreInfoMedia; userMedia?: MediaReview }): JSX.Element {
+  const { language, plot, runtime, runYears, imdbID, totalSeasons, genre, imdbRating, releaseDate, actors } = media;
+
+  const { userRating, toWatchNotes, reviewNotes } = userMedia ?? {};
 
   return (
     <div className={style.moreInfoContainer}>
-      <section className={style.posterAndMainInfoContainer}>
-        <div className={style.posterInMoreContainer}>
-          <img src={posterUrl || defaultPoster} alt={`${title} poster`} className={style.posterInMore} />
-        </div>
-        <div className={style.mainInfoContainer}>
-          <p className={style.releaseDate}>
-            <span className={style.mediaInfoLabel}>Release: </span>
-            {runYears && totalSeasons ? runYears : releaseDate}
-            {totalSeasons && (
-              <span className={style.totalSeasons}>
-                ({totalSeasons} season{totalSeasons > 1 ? 's' : ''})
-              </span>
-            )}
-          </p>
-          <p className={style.runtime}>
-            <span className={style.mediaInfoLabel}>Runtime: </span>
-            {runtime ?? '-'} min
-          </p>
-          <p className={style.languages}>
-            <span className={style.mediaInfoLabel}>Languages: </span>
-            {language && language.join(', ')}
-          </p>
-          <p className={style.imdbRating}>
-            <Link
-              href={`https://imdb.com/title/${imdbID}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              underline="none"
-              className={style.imdbLink}
-            >
-              IMDB:
-            </Link>
-            <span className={style.imdbRatingValue}> {imdbRating ?? '-'}</span>
-            /10
-          </p>
-          <p className={style.genre}>
-            <span className={style.mediaInfoLabel}>Genre:{'  '}</span>
-            {genre && genre.join(', ')}
-          </p>
-        </div>
-      </section>
-      <section className={style.otherInfoContainer}>
-        <span className={style.mediaInfoLabel}>Plot:</span>
-        <p className={style.plot}>{plot || '-'}</p>
-        <span className={style.mediaInfoLabel}>Cast:</span>
-        <p className={style.actors}>{actors && actors.join(', ')}</p>
-      </section>
-    </div>
-  );
-}
-
-function MediaReviewCard({ userMedia }: MediaReviewCardProps): JSX.Element {
-  const { userRating, toWatchNotes, reviewNotes } = userMedia ?? {};
-  return (
-    <div className={style.reviewCardContainer}>
+      <p className={style.imdbRating}>
+        <Link
+          href={`https://imdb.com/title/${imdbID}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          underline="none"
+          className={style.imdbLink}
+        >
+          IMDB:
+        </Link>
+        <span className={style.imdbRatingValue}> {imdbRating ?? '-'}</span>
+        /10
+      </p>
+      <span className={style.mediaInfoLabel}>Release: </span>
+      <p className={style.releaseDate}>
+        {runYears && totalSeasons ? runYears : releaseDate}{' '}
+        {totalSeasons && (
+          <span className={style.totalSeasons}>
+            ({totalSeasons} season{totalSeasons > 1 ? 's' : ''})
+          </span>
+        )}
+      </p>
+      <span className={style.mediaInfoLabel}>Runtime: </span>
+      <p className={style.runtime}>{runtime ?? '-'} min</p>
+      <span className={style.mediaInfoLabel}>Plot:</span>
+      <p className={style.plot}>{plot || '-'}</p>
+      <span className={style.mediaInfoLabel}>Cast:</span>
+      <p className={style.actors}>{actors && actors.join(', ')}</p>
+      <span className={style.mediaInfoLabel}>Genre:{'  '}</span>
+      <p className={style.genre}>{genre && genre.join(', ')}</p>
+      <span className={style.mediaInfoLabel}>Languages: </span>
+      <p className={style.languages}>{language && language.join(', ')}</p>
       <span className={style.mediaInfoLabel}>Watch notes:</span>
       <p className={style.toWatchNotes}>{toWatchNotes || '-'}</p>
       <span className={style.mediaInfoLabel}>Your review:</span>
@@ -115,7 +60,9 @@ function MediaReviewCard({ userMedia }: MediaReviewCardProps): JSX.Element {
         <Rating name="read-only" value={userRating} readOnly max={10} className={style.userRating} />
         <span className={style.userRatingValue}>{userRating ?? '-'}/10</span>
       </div>
+      <span className={style.mediaInfoLabel}>Review Notes:</span>
       <p className={style.reviewNotes}>{reviewNotes ?? '-'}</p>
+      <br />
     </div>
   );
 }
--- src/components/moreInfo/types.ts
+++ src/components/moreInfo/types.ts
@@ -1,5 +1,3 @@
-import { SyntheticEvent } from 'react';
-
 export type MoreInfoMedia = {
   title?: string;
   imdbID: string;
@@ -17,21 +15,11 @@ export type MoreInfoMedia = {
 export type MoreInfoCardProps = {
   hideReviewTab?: boolean;
   media: MoreInfoMedia;
-  userMedia: MediaReview;
+  userMedia?: MediaReview;
 };
-export type MoreInfoTabs = 'info' | 'review';
-export enum MoreInfoTabsDisplay {
-  info = 'More Info',
-  review = 'Notes & Review',
-}
-export type HandleTabChange = (event: SyntheticEvent, newTab: MoreInfoTabs) => void;
 
 export type MediaReview = {
   userRating?: number;
   toWatchNotes?: string;
   reviewNotes?: string;
-};
-
-export type MediaReviewCardProps = {
-  userMedia?: MediaReview;
 };
